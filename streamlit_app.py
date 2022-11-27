d26f51ef3cf77d45945ca84bb400cbc8acbd3713
MellyMel Melnyk <118758593+SFMorrisey@users.noreply.github.com>
2022-11-27 10:04:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(my_fruit_list)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # REMOVE streamlit.text(fruityvice_response.json()) # Just writes the data to the screen
 
 # write your own comment -what does the next line do? 
